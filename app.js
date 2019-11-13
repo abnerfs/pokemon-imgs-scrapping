58bc4be4c2047a2230a5ec4ef761c2a82804790a
abnerfs <abnerfs@gmail.com>
2019-11-12 22:05:00 -0300

    Improving code

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const fs = require('fs');
 const baseUrl = "https://pokemondb.net";
 const path = require('path');
 
+const DEST = './images/';
+
+if (!fs.existsSync(DEST))
+    fs.mkdirSync(DEST);
+
 const requestPromise = (url) => {
     return new Promise((resolve, reject) => {
         request(url, function (error, response, body) {
-            if(error)
+            if (error)
                 reject(error);
 
             resolve({ body, response });
@@ -16,60 +21,44 @@ const requestPromise = (url) => {
     });
 }
 
-String.prototype.capitalize = function() {
+String.prototype.capitalize = function () {
     return this.charAt(0).toUpperCase() + this.slice(1)
-  }
-
-
-function downloadImage(url, dest) {
-        var r = request(url);
-
-
-
-        r.on('response',  function (res) {
-            res.pipe(fs.createWriteStream(dest));
-        });
-
-        // requestPromise(url, function({response}) {
-        //     console.log("done " + dest);
-        //     resolve();
-        //     response.pipe(file);
-        // })
-
-        // file.on('finish', function() {
-        //     file.close(cb); 
-        // })
-        // .on('error', function(err) { // Handle errors
-        //     fs.unlink(dest); // Delete the file async. (But we don't check the result)
-        //     console.log(err);
-        //     reject(err);
-        // });
 }
 
-function obterConteudo() {
+function downloadImage(url, dest) {
+    var r = request(url);
+
+    r.on('response', function (res) {
+        res.pipe(fs.createWriteStream(dest));
+    });
+
+}
+
+function getImages() {
     const url = baseUrl + `/pokedex/all`;
 
 
     requestPromise(url)
-        .then(async ({body}) => {
+        .then(async ({ body }) => {
             const dom = new JSDOM(body);
 
             const pokemons = dom.window.document.querySelectorAll('#pokedex > tbody > tr');
             console.log(pokemons.length);
-            for(const pokemon of pokemons) {
+            for (const pokemon of pokemons) {
 
                 const urlPokemon = baseUrl + pokemon.querySelector('.ent-name').href;
-                const pokeName =   urlPokemon.replace(baseUrl + "/pokedex/", "").capitalize();
+                const pokeName = urlPokemon.replace(baseUrl + "/pokedex/", "").capitalize();
 
                 console.log(pokeName);
                 console.log(urlPokemon);
-                
+
                 const pokePage = (await requestPromise(urlPokemon)).body
                 const domPage = new JSDOM(pokePage).window.document;
                 const imgUrl = domPage.querySelector('img').src;
 
                 const ext = path.extname(imgUrl);
-                const dest = "./images/" + pokeName + ext;
+
+                const dest = `${DEST}${pokeName}${ext}`;
 
                 console.log(imgUrl);
                 console.log(dest);
@@ -79,8 +68,7 @@ function obterConteudo() {
         })
         .catch(error => {
             console.log(error);
-
         })
 }
 
-obterConteudo();
+getImages();
